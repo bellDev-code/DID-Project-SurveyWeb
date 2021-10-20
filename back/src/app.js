9daa689268b410670e7677ed1867b7b07c865b38
bellDev-code <sumaoo20@naver.com>
2021-10-20 17:49:46 +0900

    DB connecting...

--- back/src/app.js
+++ back/src/app.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const db = require("./db");
 
 const app = express();
+
+const test = async () => {
+  db.connect();
+
+  db.query("SELECT NOW()", (err, res) => {
+    console.log(err, res);
+  });
+
+  db.end();
+};
+
+test();
 
 // Set port
 app.set("PORT", process.env.PORT || 4190);
---
+++ back/src/db/index.js
@@ -0,0 +1,15 @@
+// 환경 변수
+require("dotenv").config();
+
+// pg client 생성
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_DATABASE_NAME,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+});
+
+module.exports = pool;

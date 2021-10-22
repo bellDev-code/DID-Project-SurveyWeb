ed54e105e7af1ad26556ad00cd50e2895c22c1f7
bellDev-code <sumaoo20@naver.com>
2021-10-22 14:52:44 +0900

    DB Connect and Create User Table and Router

--- back/src/app.js
+++ back/src/app.js
@@ -2,22 +2,27 @@ const express = require("express");
 const db = require("./db");
 
 const app = express();
+app.use(express.json());
 
 // db connecting test
-const test = async () => {
-  db.connect();
+// const test = async () => {
+//   db.connect();
 
-  db.query("SELECT NOW()", (err, res) => {
-    console.log(err, res);
-  });
+//   db.query("SELECT NOW()", (err, res) => {
+//     console.log(err, res);
+//   });
 
-  db.end();
-};
+//   db.end();
+// };
 
-test();
+// test();
 
 // Set port
 app.set("PORT", process.env.PORT || 4190);
+
+const api = require("./routers");
+
+app.use("/api", api);
 
 // Server Start
 app.listen(app.get("PORT"), () => {
--- back/src/db/index.js
+++ back/src/db/index.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 // pg client 생성
 const { Pool } = require("pg");
 
+// AWS RDS 문제 해결 DB 연결할 때, 문제 생기면
+// console.log(process.env) 확인하기
 const pool = new Pool({
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
@@ -11,5 +13,30 @@ const pool = new Pool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
 });
+
+const initialize = async () => {
+  // DB 연결
+  const client = await pool.connect();
+
+  // User 테이블 만드는 쿼리문
+  await client.query(
+    `
+    CREATE TABLE IF NOT EXISTS public."User"(
+      id          SERIAL      PRIMARY KEY,
+      username    TEXT        NOT NULL     UNIQUE,
+      password    TEXT        NOT NULL,
+      name        TEXT        NOT NULL,
+      email       TEXT        NOT NULL     UNIQUE,
+      "createdAt" TIMESTAMP   NOT NULL,
+      "updatedAt" TIMESTAMP   NOT NULL
+    )
+    `
+  );
+
+  // 디비 연결 종료
+  await client.release();
+};
+
+initialize();
 
 module.exports = pool;
